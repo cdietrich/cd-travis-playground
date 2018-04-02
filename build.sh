3be9d3efa3e8b6a3124cfddf5203a113a62d3dec
Christian Dietrich <christian.dietrich@itemis.de>
2018-04-02 19:01:43 +0200

    more stuff
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 git stash
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "mac"
-	sed_inplace="''"
-else
-	echo "linux"
-	sed_inplace=""
-fi
+
+
+sed_inplace() {
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "mac"
+		sed -i '' "$@"
+	else
+		echo "linux"
+		sed -i "$@" 
+	fi	
+}
+
 echo "sed -i $sed_inplace s/a/b/g lala.txt"
-sed -i $sed_inplace s/a/b/g lala.txt
+sed_inplace s/a/b/g lala.txt
 ls -la
 cat lala.txt
-rm "lala.txt''"
-sed -i '' s/a/b/g lala.txt
-ls -la
 git stash
